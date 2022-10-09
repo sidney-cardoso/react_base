6d40f229c6166bf18e5986b7b266aa775fd56864
Sidney Cardoso <sidneyrsc342@gmail.com>
2022-10-09 10:15:19 -0300

    Menu da aplicação

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+
 import Login from './pages/Login'
 import GlobalStyles from "./styles/global";
+import Header from "./components/Header";
 
 export default () => {
   return (
     <>
+      <Header />
       <Login />
       <GlobalStyles />
     </>
